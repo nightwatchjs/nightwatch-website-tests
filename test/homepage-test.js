9eba3f31711d20c87632d427ed0c976c91a88662
Vaibhav Singh <singh.vaibhav2011@gmail.com>
2021-05-17 21:51:39 +0530

    Fix: homepage test

--- test/homepage-test.js
+++ test/homepage-test.js
@@ -9,21 +9,12 @@ module.exports = {
     this.homepage.expect.section('@indexContainer').to.be.visible;
   },
 
-  'check if version dropdown is enabled and contains the correct version': function (client) {
-    const navigation = this.homepage.section.navigation;
+  'check if version dropdown is enabled and contains the correct version': function () {
     const navbarHeader = this.homepage.section.navbarHeader;
 
-    // TODO: fix page object related issues
     this.homepage.expect.section('@navbarHeader').to.be.enabled;
-    navbarHeader.expect.element('@versionDropdown').to.be.enabled;
-    navbarHeader.expect.element('@versionDropdownOption').to.be.enabled;
-    navbarHeader.getText('css selector', '@versionDropdown', (res) => console.log(res));
-
-    // navbarHeader.click('@versionDropdown');
-    // this.homepage.expect.section('@navbarHeader').to.be.visible;
-    // navbarHeader.expect.element('@versionDropdown').to.be.enabled;
-    // navbarHeader.expect.element('@versionDropdownOption').to.be.enabled;
-    // navbarHeader.expect.element('.nav-item.dropdown').text.to.equal(client.globals.nightwatchVersion);
+    navbarHeader.expect.element('@versionDropdown').to.be.visible;
+    navbarHeader.expect.element('@versionDropdown').text.to.equal('1.6.2');
   },
 
   after(client) {
